cea8b12eb0cd032e7d7adbc5d416be14657af55d
guna1202 <guna120202@gmail.com>
2023-11-01 16:04:31 +0530

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
   try {
@@ -23,6 +24,32 @@ export const deleteListing = async (req, res, next) => {
   try {
     await Listing.findByIdAndDelete(req.params.id);
     res.status(200).json("Listing has been deleted!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return res.status(400).json({
+        success: false,
+        message: "Listing not found",
+      });
+    }
+    if (req.user.id !== listing.userRef) {
+      return res.status(400).json({
+        success: false,
+        message: "You can only update your listing",
+      });
+    }
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
   } catch (error) {
     next(error);
   }
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -31,7 +31,7 @@ const Profile = () => {
   const [formData, setFormData] = useState({});
   const [fileUploadError, setFileUploadError] = useState(false);
   const [updateSuccess, setUpdateSuccess] = useState(false);
-  const [listingsError, setListingsError] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
   const [userListings, setUserListings] = useState([]);
 
   useEffect(() => {
@@ -126,17 +126,17 @@ const Profile = () => {
 
   const handleShowListings = async () => {
     try {
-      setListingsError(false);
+      setShowListingsError(false);
       const res = await fetch(`/api/user/listings/${currentUser._id}`);
       const data = await res.json();
       if (data.success === false) {
-        setListingsError(true);
+        setShowListingsError(true);
         return;
       }
-      setListingsError(false);
+      setShowListingsError(false);
       setUserListings(data);
     } catch (error) {
-      setListingsError(error.message);
+      setShowListingsError(error.message);
     }
   };
 
@@ -158,35 +158,32 @@ const Profile = () => {
 
   return (
     <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl my-7 text-green-500 text-center font-bold">
+      <h1 className="text-3xl text-green-500 font-bold text-center my-7">
         Profile
       </h1>
-
-      <form onSubmit={handleSubmit} className="flex flex-col gap-5">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           onChange={(e) => setFile(e.target.files[0])}
           type="file"
           ref={fileRef}
-          accept="image/*"
           hidden
+          accept="image/*"
         />
         <img
-          onChange={(e) => setFile(e.target.files[0])}
           onClick={() => fileRef.current.click()}
           src={formData.avatar || currentUser.avatar}
           alt="profile"
-          className="rounded-full h-24 w-24 object cover cursor-pointer self-center mt-2"
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
         />
-        <p className="text-center">
+        <p className="text-sm self-center">
           {fileUploadError ? (
             <span className="text-red-700">
-              {" "}
-              Error occurred (Image must be less than 2MB)
+              Error Image upload (image must be less than 2 mb)
             </span>
           ) : filePerc > 0 && filePerc < 100 ? (
-            <span className="text-slate-700"> {`Uploading ${filePerc}%`}</span>
+            <span className="text-slate-700">{`Uploading ${filePerc}%`}</span>
           ) : filePerc === 100 ? (
-            <span className="text-green-600">Image uploaded successfully</span>
+            <span className="text-green-700">Image successfully uploaded!</span>
           ) : (
             ""
           )}
@@ -194,9 +191,9 @@ const Profile = () => {
         <input
           type="text"
           placeholder="username"
+          defaultValue={currentUser.username}
           id="username"
-          defaultValue={formData.avatar || currentUser.username}
-          className="border bg-slate-200 p-3 rounded-lg"
+          className="border p-3 rounded-lg"
           onChange={handleChange}
         />
         <input
@@ -204,55 +201,52 @@ const Profile = () => {
           placeholder="email"
           id="email"
           defaultValue={currentUser.email}
-          className="border bg-slate-200 p-3 rounded-lg"
+          className="border p-3 rounded-lg"
           onChange={handleChange}
         />
         <input
           type="password"
           placeholder="password"
-          id="password"
-          className="border bg-slate-200 p-3 rounded-lg"
           onChange={handleChange}
+          id="password"
+          className="border p-3 rounded-lg"
         />
         <button
           disabled={loading}
-          className="uppercase bg-slate-700 p-3 rounded-lg text-white font-semibold hover:opacity-80 disabled:opacity-60"
+          className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80"
         >
-          {loading ? "loading..." : "update"}
+          {loading ? "Loading..." : "Update"}
         </button>
         <Link
+          className="bg-green-500 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
           to={"/create-listing"}
-          className="uppercase bg-green-500 p-3 rounded-lg text-white font-semibold text-center hover:opacity-80"
         >
           Create Listing
         </Link>
       </form>
-      <div className=" flex justify-between mt-5">
-        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">
-          Sign Out
-        </span>
+      <div className="flex justify-between mt-5">
         <span
           onClick={handleDeleteUser}
           className="text-red-700 cursor-pointer"
         >
           Delete account
         </span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">
+          Sign out
+        </span>
       </div>
-      {error ? (
-        <p className="text-red-700 mt-5">{error}</p>
-      ) : updateSuccess ? (
-        <p className="text-green-700 mt-5"> User updated successfully!</p>
-      ) : (
-        ""
-      )}
-      <button onClick={handleShowListings} className="text-green-500 w-full">
+      <p className="text-red-700 mt-5">{error ? error : ""}</p>
+      <p className="text-green-700 mt-5">
+        {updateSuccess ? "User is updated successfully!" : ""}
+      </p>
+      <button onClick={handleShowListings} className="text-green-700 w-full">
         Show Listings
       </button>
       <p className="text-red-700 mt-5">
-        {listingsError ? "Error showing listings" : ""}
+        {showListingsError ? "Error showing listings" : ""}
       </p>
       {userListings && userListings.length > 0 && (
-        <div className="">
+        <div className="flex flex-col gap-4">
           <h1 className="text-center mt-7 text-2xl font-semibold">
             Your Listings
           </h1>
@@ -261,7 +255,7 @@ const Profile = () => {
               key={listing._id}
               className="border rounded-lg p-3 flex justify-between items-center gap-4"
             >
-              <Link to={`/listings/${listing._id}`}>
+              <Link to={`/listing/${listing._id}`}>
                 <img
                   src={listing.imageUrls[0]}
                   alt="listing cover"
@@ -269,21 +263,22 @@ const Profile = () => {
                 />
               </Link>
               <Link
-                to={`/listings/${listing._id}`}
-                className="text-slate-700 font-semibold hover:underline truncate flex-1"
+                className="text-slate-700 font-semibold  hover:underline truncate flex-1"
+                to={`/listing/${listing._id}`}
               >
                 <p>{listing.name}</p>
               </Link>
-              <div className="flex flex-col items-center">
+
+              <div className="flex flex-col item-center">
                 <button
                   onClick={() => handleListingDelete(listing._id)}
-                  className="text-red-700 uppercase text-sm"
+                  className="text-red-700 uppercase"
                 >
                   Delete
                 </button>
-                <button className="text-green-700 uppercase text-sm">
-                  Edit
-                </button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className="text-green-700 uppercase">Edit</button>
+                </Link>
               </div>
             </div>
           ))}
